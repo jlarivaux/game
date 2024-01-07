84e83410597f888017673dc7c84edd4e59478dce
jlarivaux <jplarivaux@gmail.com>
2024-01-07 10:21:36 -0800

    mastermind and battleship

--- battleship/jeremiah_larivaux_battleship_project/lib/player.rb
+++
@@ -1,9 +0,0 @@
-class Player
-
-    def get_move
-        puts "enter a position with coordinates separated with a space like `4 7`"
-        coordinates = gets.chomp.split.map(&:to_i)
-        
-    end
-end
-
